9bc041a3494261b0b1c1b61a149b147747583d61
John Regehr <regehr@cs.utah.edu>
2021-02-03 11:31:08 -0700

    foo

--- assignment1/lexer-tests2/gen.cpp
+++ assignment1/lexer-tests2/gen.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(void) {
-  for (int i=0; i<=255; i++) {
+  for (int i = 0; i <= 255; i++) {
     char fn[256];
     const char *dir = ((i >= 32 && i <= 126) && i != 34) ? "ok" : "error";
     sprintf(fn, "%s/print_%03d.jpl", dir, i);
@@ -14,13 +14,11 @@ int main(void) {
 
   // FIXME both ones below aren't right-- they correctly classify for
   // parsing but not lexing
-  
-  for (int i=0; i<=255; i++) {
+
+  for (int i = 0; i <= 255; i++) {
     char fn[256];
     const char *dir =
-      ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z')) ?
-      "ok" :
-      "error";
+        ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z')) ? "ok" : "error";
     sprintf(fn, "%s/variable_%03d.jpl", dir, i);
     FILE *f = fopen(fn, "w");
     assert(f);
@@ -28,18 +26,12 @@ int main(void) {
     fclose(f);
   }
 
-  for (int i=0; i<=255; i++) {
+  for (int i = 0; i <= 255; i++) {
     char fn[256];
-    const char *dir =
-      (
-       (i >= 'a' && i <= 'z') ||
-       (i >= 'A' && i <= 'Z') ||
-       (i >= '0' && i <= '9') ||
-       (i == '_') ||
-       (i == '.')
-       ) ?
-      "ok" :
-      "error";
+    const char *dir = ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') ||
+                       (i >= '0' && i <= '9') || (i == '_') || (i == '.'))
+                          ? "ok"
+                          : "error";
     sprintf(fn, "%s/in_variable_%03d.jpl", dir, i);
     FILE *f = fopen(fn, "w");
     assert(f);
