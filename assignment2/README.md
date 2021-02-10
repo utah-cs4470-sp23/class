446e796dcfce4ca6cd4becde8ea616d94f7e7a7e
Pavel Panchekha <me@pavpanchekha.com>
2021-02-10 11:03:16 -0700

    Stop at 'Compilation

--- assignment2/README.md
+++ assignment2/README.md
@@ -134,7 +134,7 @@ Racket program:
 
 ``` {.racket}
 #lang racket
-(for ([line (in-port read)])
+(for ([line (in-port read)] #:break (equal? line 'Compilation))
   (pretty-print line (current-output-port) 1))
 ```
 
--- assignment2/pp.rkt
+++ assignment2/pp.rkt
@@ -3,5 +3,5 @@
 ;; make an executable version of this like so:
 ;; raco exe pp.rkt
 
-(for ([line (in-port read)])
+(for ([line (in-port read)] #:break (equal? line 'Compilation))
   (pretty-print line (current-output-port) 1))
