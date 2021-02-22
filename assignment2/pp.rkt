ad9bf9701a93ed8256236d49f023827065f1ff8b
Emerson Ford <emersontford@gmail.com>
2021-02-21 19:08:51 -0700

    Fix pretty-print program incorrectly parsing backslashes

--- assignment2/pp.rkt
+++ assignment2/pp.rkt
@@ -1,7 +1,11 @@
 #lang racket
 
-;; make an executable version of this like so:
-;; raco exe pp.rkt
-
-(for ([line (in-port read)] #:break (equal? line 'Compilation))
-  (pretty-print line (current-output-port) 1))
+; Read stdin as string > escape backslashes > parse as s-expressions > pretty print as
+; string > remove backslash escapes
+(for ([line (in-port read-line)] #:break (string-prefix? line "Compilation"))
+     (define op (open-output-string))
+     ; Use pretty-print for quote-depth functionality.
+     ; Escape backslashes so read interprets them as literal backslashes.
+     (pretty-print (read (open-input-string (string-replace line "\\" "\\\\"))) op 1)
+     ; Remove the escapes from before.
+     (display (string-replace (get-output-string op) "\\\\" "\\")))
