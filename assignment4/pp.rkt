45831e5b3fdefab708866c741361075f59c3c3d7
Pavel Panchekha <me@pavpanchekha.com>
2021-03-01 11:45:21 -0700

    Fixed pp

--- assignment4/pp.rkt
+++ assignment4/pp.rkt
@@ -1,13 +1,8 @@
 #lang racket
 
-;; Read stdin as string > escape backslashes > parse as s-expressions > pretty print as
-;; string > remove backslash escapes
-(for ([line (in-port read-line)] #:break (string-prefix? line "Compilation"))
-     (define op (open-output-string))
-     (define val (read (open-input-string (string-replace line "\\" "\\\\"))))
-     (unless (eof-object? val)
-       ;; Use pretty-print for quote-depth functionality.
-       ;; Escape backslashes so read interprets them as literal backslashes.
-       (pretty-print val op 1)
-       ;; Remove the escapes from before.
-       (display (string-replace (get-output-string op) "\\\\" "\\"))))
+;; Preprocess output to escape all backslashes
+(define s (port->string (current-input-port)))
+(define port (open-input-string (string-replace s "\\" "\\\\")))
+
+(for ([val (in-port read port)] #:break (eq? val 'Compilation))
+  (pretty-print val (current-output-port) 1))
